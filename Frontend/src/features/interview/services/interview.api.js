67e186effc9036312d71e45c648ca5df9336ad44
Ankita <ankitamishra9302@gmail.com>
2026-05-03 15:10:09 +0530

    fix interview api url

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://interview-ai-yt-9z4z.onrender.com",
+    
+    //baseURL: "http://localhost:3000",
     withCredentials: true,
 })
 
